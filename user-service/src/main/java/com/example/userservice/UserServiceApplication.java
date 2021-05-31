2f5a5bc38d42c201681933018a06f81da2638e43
赖钦祥 <1979785764@qq.com>
2021-05-31 13:25:42 +0800

    user-service启动成功

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -2,12 +2,16 @@ package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(UserServiceApplication.class, args);
+		ConfigurableEnvironment environment = context.getEnvironment();
+		System.out.println(environment.getProperty("spring.datasource.username"));
 	}
 
 }

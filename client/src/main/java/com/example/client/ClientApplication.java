cf7b978f3e8dd9824fde2868af0dfc1c13709c69
Angenela <2392429828@qq.com>
2021-06-02 19:33:15 +0800

    Kafka添加成功

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -2,18 +2,12 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
-@PropertySource(value = {"classpath:kafka.properties"})
 public class ClientApplication {
 
-    public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(ClientApplication.class, args);
-        ConfigurableEnvironment environment = context.getEnvironment();
-//        System.err.println(environment.getProperty("spring.kafka.bootstrap-servers"));
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+
 }
